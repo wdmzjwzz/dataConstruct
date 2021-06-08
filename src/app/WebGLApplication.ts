291665048039fd2105c94146181ef7f33edfab34
wangzhizhi <2216453885@qq.com>
2021-06-08 21:25:26 +0800

    texture

--- src/app/WebGLApplication.ts
+++ src/app/WebGLApplication.ts
@@ -1,7 +1,9 @@
 import { GLProgram } from "../GLProgram/GLProgram";
 import { GLProgramCache } from "../GLProgram/GLProgramCache";
 import { Application } from "./Application";
-
+import { GLMeshBuilder } from "../GLMesh/GLMeshBuilder"
+import mat4 from "../math/mat4";
+import { vec3 } from "../math/tsm";
 export class WebGLApplication extends Application {
     constructor(canvas: HTMLCanvasElement) {
         super(canvas);
@@ -22,8 +24,17 @@ export class WebGLApplication extends Application {
         this.canvas.addEventListener("webglcontextlost", (e) => {
             console.log(JSON.stringify(e), "webglcontextlost")
         }, false)
-        GLProgramCache.instance.set("color", GLProgram.createDefaultColorProgram(this.gl));
-        GLProgramCache.instance.set("texture", GLProgram.createDefaultTextureProgram(this.gl));
+        // let projectMatrix = mat4.perspective(1, this.canvas.width / this.canvas.height, 0.1, 1000)
+        // 视矩阵
+        // let viewMatrix = mat4.lookAt(new vec3([0, 0, 200]), new vec3())
+        // let viewProjectMatrix = mat4.product(projectMatrix, viewMatrix, new mat4())
+        // const colorP = GLProgram.createDefaultColorProgram(this.gl)
+        // console.log(colorP,111)
+        // GLProgramCache.instance.set("color", colorP);
+        // const mesh = new GLMeshBuilder(this.gl, colorP._attribState, GLProgramCache.instance.getMaybe("color"),)
+        // mesh.color(0, 0, 0, 1).vertex(0.5, 0.5, 0).vertex(-0.5, -0.5, 0).vertex(0.5, -0.5, 0).begin(this.gl.TRIANGLES).end(viewProjectMatrix)
+        // GLProgramCache.instance.set("texture", GLProgram.createDefaultTextureProgram(this.gl));
+
     }
     public gl: WebGLRenderingContext | null = null;
     public matStack: any;
