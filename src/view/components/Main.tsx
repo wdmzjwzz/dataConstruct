627a709e345c06c8b323a21eb5181c798d4ba7ba
wangzhizhi <2216453885@qq.com>
2021-12-18 17:29:25 +0800

    111

--- src/view/components/Main.tsx
+++ src/view/components/Main.tsx
@@ -1,24 +1,24 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./main.less";
 import "../assets/font/iconfont.css";
-import { RotatingCubeApplication } from "../../controller/RotatingCubeApplication";
+import { MeshBuilderApplicaton } from "../../controller/MeshBuilderApplication";
 
 export default () => {
   const [fps, setFps] = useState<number>(0);
   const canvas = useRef<HTMLCanvasElement>(null);
   const resizeFun = () => {
-    canvas.current?.setAttribute("width", document.body.clientWidth - 480 + "");
+    canvas.current?.setAttribute("width", document.body.clientWidth + "");
     canvas.current?.setAttribute(
       "height",
       document.body.clientHeight - 40 + ""
     );
   };
-  let app: RotatingCubeApplication;
+  let app: MeshBuilderApplicaton;
   const frameCallback = () => {
     setFps(Math.ceil(app.fps));
   };
   const init = () => {
-    app = new RotatingCubeApplication(canvas.current);
+    app = new MeshBuilderApplicaton(canvas.current);
     app.addTimer(frameCallback, 1);
     app.run();
   };
