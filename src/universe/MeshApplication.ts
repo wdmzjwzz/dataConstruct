6480084ad5c5a28be48e1b35523ab23f3217d4f1
wangzhizhi <2216453885@qq.com>
2022-05-01 13:06:24 +0800

    111

--- src/universe/MeshApplication.ts
+++ src/universe/MeshApplication.ts
@@ -5,21 +5,24 @@ import {
   CanvasKeyBoardEvent,
   CanvasMouseEvent,
 } from "./lib/Application";
-import { DrawHelper, GLProgram, GLTexture } from ".";
+import { DrawHelper, GLProgram, GLTexture, HttpRequest } from ".";
 import { Point } from "./Geometry/Point";
 import { GLHelper } from "./webgl/WebGLHepler";
 import { GLShaderType } from "./webgl/glsl";
 import { GLAttribName } from "./type";
+import { GLAttribStateManager } from "./webgl/WebGLAttribState";
 export class MeshApplication extends CameraApplication {
   public textureBuilder: GLMeshBuilder;
-  public textureShader: GLProgram; // 纹理着色器
-  public texture: GLTexture; // 纹理着色器所使用的纹理对象
-
+  public textures: GLTexture[] = []; // 纹理着色器所使用的纹理对象
+  public currTexIdx: number = 0
+  public textureProgram: GLProgram; // 使用颜色GPU Program对象
   public constructor(canvas: HTMLCanvasElement) {
     super(canvas, { premultipliedAlpha: false });
     this.camera.setViewport(0, 0, this.canvas.width, this.canvas.height);
-    this.textureBuilder = GLHelper.createBuilder(this.gl, GLShaderType.TEXTURE,[GLAttribName.POSITION, GLAttribName.COLOR,GLAttribName.TEXCOORD]);
+    this.textureBuilder = GLHelper.createBuilder(this.gl, GLShaderType.TEXTURE, [GLAttribName.POSITION, GLAttribName.COLOR, GLAttribName.TEXCOORD]);
     this.isSupportMouseMove = true;
+    const bit = GLAttribStateManager.makeVertexAttribs([GLAttribName.POSITION, GLAttribName.COLOR, GLAttribName.TEXCOORD]);
+    this.textureProgram = GLProgram.createProgram(GLShaderType.TEXTURE, this.gl, bit)
   }
   public onMouseMove(evt: CanvasMouseEvent): void {
     if (this._isMouseDown) {
@@ -44,7 +47,40 @@ export class MeshApplication extends CameraApplication {
     DrawHelper.drawCoordSystem(this.builder, Matrix4.m0, 5);
     this.matStack.popMatrix();
   }
+  public async run(): Promise<void> {
+    // await必须要用于async声明的函数中
+    let img: HTMLImageElement = await HttpRequest.loadImageAsync(
+      "data/pic0.png"
+    );
+    let tex: GLTexture = new GLTexture(this.gl);
+    tex.upload(img, 0, true);
+    tex.filter();
+    this.textures.push(tex);
+    console.log("1、第一个纹理载入成功！");
+
+    // await必须要用于async声明的函数中
+    img = await HttpRequest.loadImageAsync("data/pic1.jpg");
+    tex = new GLTexture(this.gl);
+    tex.upload(img, 0, true);
+    tex.filter();
+    this.textures.push(tex);
+    console.log("2、第二个纹理载入成功！");
+
+    // 在资源同步加载完成后，直接启动换肤的定时器，每隔2秒，将立方体的皮肤进行周而复始的替换
+    this.addTimer(this.cubeTimeCallback.bind(this), 2, false);
+
+    console.log("3、启动Application程序");
+    super.run(); // 调用基类的run方法，基类run方法内部调用了start方法
+  }
+  public cubeTimeCallback(id: number, data: any): void {
+    this.currTexIdx++; // 定时让计数器+1
+    // 取模操作，让currTexIdx的取值范围为[ 0, 2 ]之间（当前有3个纹理）
+    this.currTexIdx %= this.textures.length;
+  }
   public drawByMatrixWithColorShader(): void {
+    this.textures[this.currTexIdx].bind();
+    this.textureProgram.bind();
+    this.textureProgram.loadSampler();
     this.matStack.pushMatrix(); // 矩阵堆栈进栈
 
     this.matStack.rotate(-this.angle, new Vector3([0, 1, 0]).normalize());
@@ -63,7 +99,7 @@ export class MeshApplication extends CameraApplication {
     );
 
     DrawHelper.drawSolidCubeBox(this.builder, Matrix4.m0);
- 
+
     this.matStack.popMatrix();
   }
 
@@ -76,6 +112,7 @@ export class MeshApplication extends CameraApplication {
     this.gl.disable(this.gl.CULL_FACE);
 
     this.gl.enable(this.gl.DEPTH_TEST);
+
     this.drawCoordSystem();
     this.drawByMatrixWithColorShader();
     // 恢复三角形背面剔除功能
